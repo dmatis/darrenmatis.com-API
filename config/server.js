019a0ed19fd3e8284e2c21f2280225c9fbe7f7d7
Darren Matis <darren.matis@sap.com>
2020-12-04 13:31:45 -0800

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b89e70ac7a762c258dfe79a15372e828'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f61fcef8-099a-4e79-8e0e-898c08fabb32'
+};
